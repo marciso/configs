387ab0d463f1507bf64d60c76844d4125a6d8392
Marcin Sobieszczanski <marcin@taniustech.com>
2019-10-04 22:34:21 +0000

    fix copy-my-env.sh

--- scripts/copy-my-env.sh
+++ scripts/copy-my-env.sh
@@ -7,28 +7,31 @@ test -n "$HOST" || die "Specify host to copy to"
 
 declare -a files
 files=(
-    ~/gentoo/
-    ~/bin/
-    ~/.bash_profile
-    ~/.bash_logout
-    ~/.bashrc
-    ~/.tmux.conf
-    ~/.vimrc
-    ~/.vim/
-    ~/.gitconfig
-    ~/.dircolors
-    ~/.dircolors.small
-    ~/.flake8
-    ~/.gdbinit
-    ~/.git-prompt.sh
-    ~/.ssh/config
-    ~/.zlogin
-    ~/.zprofile
-    ~/.zsh
-    ~/.zshenv
-    ~/.zshrc
+    gentoo/
+    bin/
+    .bash_profile
+    .bash_logout
+    .bashrc
+    .tmux.conf
+    .vimrc
+    .vim/
+    .gitconfig
+    .dircolors
+    .dircolors.small
+    .flake8
+    .gdbinit
+    .git-prompt.sh
+    .ssh/config
+    .zlogin
+    .zprofile
+    .zsh/
+    .zshenv
+    .zshrc
     )
 
+
+pushd $HOME
 for f in ${files[@]} ; do
-    echo rsync -varPi ~/gentoo/ $HOST:~/gentoo/
+    run_log rsync -varPi $f $HOST:$f
 done
+popd
